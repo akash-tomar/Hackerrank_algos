b61bdb4ce2f308d004e42c8882aaf0367b001628
Akash Tomar <akash.tomar217@gmail.com>
2017-03-20 12:15:15 +0530

    bug

--- QuickestWayUp.java
+++ QuickestWayUp.java
@@ -12,32 +12,43 @@ public class QuickestWayUp {
 		public Pair(int data) {
 			this.data = data;
 			weight = Integer.MAX_VALUE;
-			dice_roll = 0;
+			dice_roll = Integer.MAX_VALUE;
 			visited = false;
 		}
 	}
 
 	public static void getShortestPath(int current_index,Pair[] game,HashMap<Integer,Integer> ladder, HashMap<Integer,Integer> snakes) {
+		if(current_index==6) {
+			System.out.println(game);
+		}
+		if(game[current_index-1].visited) {
+			return;
+		}
 		if(current_index>=100) {
 			return;
 		}
 		game[current_index-1].visited=true;
 		for(int i=1;i<=6;i++) {
 			if(current_index+i<=100) {
-//				if(!game[current_index+i-1].visited) {
-					int ch = current_index+i;
-					if(ladder.containsKey(current_index+i)) {
-						ch = ladder.get(current_index+i);
-					} else if(snakes.containsKey(current_index+i)) {
-						ch = snakes.get(current_index+i);
-					}
-					game[ch-1].dice_roll=game[current_index-1].dice_roll+1;
-					int weight = game[ch-1].weight;
-					game[ch-1].weight = Math.min(weight, game[current_index-1].weight+i);
-					if(ch>current_index && !game[current_index+i-1].visited) {
+				int ch = current_index+i;
+				if(ch==12) {
+					System.out.println();
+				}
+				if(ladder.containsKey(ch)) {
+					ch = ladder.get(ch);
+				} else if(snakes.containsKey(ch)) {
+					ch = snakes.get(ch);
+				}
+				if(ch>current_index) {
+					if(game[ch-1].dice_roll > game[current_index-1].dice_roll+1) {
+						game[ch-1].dice_roll=game[current_index-1].dice_roll+1;
+						int weight = game[ch-1].weight;
+						game[ch-1].weight = Math.min(weight, game[current_index-1].weight+i);
+						//					if(ch>current_index) {
 						getShortestPath(ch, game, ladder, snakes);
+						//					}
 					}
-//				}
+				}
 			}
 		}
 	}
@@ -69,7 +80,7 @@ public class QuickestWayUp {
 				game[i-1]=new Pair(i);
 			}
 			game[0].weight=0;
-			game[0].visited=true;
+			game[0].dice_roll=0;
 			getShortestPath(1, game,ladder,snakes);
 			System.out.println(game[99].dice_roll);
 		}
