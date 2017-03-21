b13dc4cfa2681937c2129e511c9f8757efabcdfc
Akash Tomar <akash.tomar217@gmail.com>
2017-03-21 21:07:21 +0530

    timeout

---
+++ ModularGameOfNumbers.java
@@ -0,0 +1,50 @@
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ModularGameOfNumbers {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int n = s.nextInt();
+		int p = s.nextInt();
+		int q = s.nextInt();
+		int[] a = new int[p];
+		int[] b = new int[q];
+		for(int i=0;i<p;i++) {
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<q;i++) {
+			b[i]=s.nextInt();
+		}
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i=1;i<=n;i++) {
+			map.put(i,0);
+		}
+		
+		for(int i=0;i<p;i++) {
+			int first = a[i];
+			for(int j=0;j<q;j++) {
+				int second = b[j];
+				
+				int x = (first+second) % n;
+				for(int k=1;k<=n;k++) {
+					int temp = x+k;
+					if(temp%n==0) {
+						map.put(k, map.get(k)+1);
+					}
+				}
+			}
+		}
+		int min = Integer.MAX_VALUE;
+		int ans = -1;
+		for(int i=1;i<=n;i++) {
+			if(min>map.get(i)) {
+				min = map.get(i);
+				ans = i;
+			}
+		}
+		System.out.println(ans);
+	}
+}
