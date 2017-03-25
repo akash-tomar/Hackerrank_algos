d5d16f971531e33a18de91ede123c2dd10771fca
Akash Tomar <akash.tomar217@gmail.com>
2017-03-25 18:42:21 +0530

    changes to modulargame

--- ModularGameOfNumbers.java
+++ ModularGameOfNumbers.java
@@ -17,24 +17,28 @@ public class ModularGameOfNumbers {
 		for(int i=0;i<q;i++) {
 			b[i]=s.nextInt();
 		}
-		
+
 		HashMap<Integer, Integer> map = new HashMap<>();
 		for(int i=1;i<=n;i++) {
 			map.put(i,0);
 		}
-		
-		for(int i=0;i<p;i++) {
-			int first = a[i];
-			for(int j=0;j<q;j++) {
-				int second = b[j];
-				
-				int x = (first+second) % n;
-				for(int k=1;k<=n;k++) {
+		for(int k=1;k<=n;k++) {
+			boolean flag = false;
+			for(int i=0;i<p;i++) {
+				int first = a[i];
+				for(int j=0;j<q;j++) {
+					int second = b[j];
+					int x = (first+second) % n;
 					int temp = x+k;
 					if(temp%n==0) {
+						flag = true;
 						map.put(k, map.get(k)+1);
 					}
 				}
+			}
+			if(!flag) {
+				System.out.println(k);
+				return;
 			}
 		}
 		int min = Integer.MAX_VALUE;
