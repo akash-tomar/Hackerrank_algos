3e887053a97fc8f3dae588fc5416f61c9f6d9816
Akash Tomar <akash.tomar217@gmail.com>
2017-04-20 22:59:24 +0530

    accurate string

--- AccurateSorting.java
+++ AccurateSorting.java
@@ -8,21 +8,33 @@ public class AccurateSorting {
         for(int a0 = 0; a0 < q; a0++){
             int n = in.nextInt();
             int[] a = new int[n];
+            HashMap<Integer, Integer> map =new HashMap<>();
             for(int a_i=0; a_i < n; a_i++){
                 a[a_i] = in.nextInt();
+                map.put(a[a_i], a_i);
             }
+            
+            
+            int[] arr= a.clone();
+            Arrays.sort(arr);
+            int end = arr.length-1;
             boolean flag = false;
-            for(int i=0;i<n;i++) {
-            	for(int j=i+1;j<n;j++) {
-            		if(a[i]>a[j] && a[i]-a[j]>=2) {
+            HashMap<Integer, Boolean> seen = new HashMap<>();
+            while(end>=0) {
+            	int index = map.get(arr[end]);
+            	for(int i=index+1;i<n;i++) {
+            		if(Math.abs(a[i]-a[index])>=2 && !seen.containsKey(a[i])) {
             			System.out.println("No");
             			flag=true;
             			break;
-            		}
+            		} 
             	}
             	if(flag) {
             		break;
+            	} else {
+            		seen.put(a[index],true);
             	}
+            	end--;
             }
             if(flag) {
             	continue;
